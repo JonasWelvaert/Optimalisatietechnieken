5bf4748f6e2085eb7fb1927a24ecad0820adbf13
Romeo Permentier <56132002+ro-per@users.noreply.github.com>
2020-11-09 12:55:16 +0100

    update structure

--- src/Planner.java
+++ src/Planner.java
@@ -5,47 +5,78 @@ import model.Day;
 import model.Machine;
 
 public class Planner {
-	private static List<Machine> machines;
-	private static List<Day> days;
+    private static List<Machine> machines;
+    private static List<Day> days;
 
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-		System.out.println("#Verwijder jouw lijn als je kan bewerken op GIT.");
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("#Verwijder jouw lijn als je kan bewerken op GIT.");
 //		System.out.println("\t Romeo Permentier");
-		System.out.println("\tNick Braeckman");
-		System.out.println("\tElke Govaert");
+        System.out.println("\tNick Braeckman");
+        System.out.println("\tElke Govaert");
 
-		ReadFileIn();
 
-		// metaheuristik();
+        // 1. Initiele oplossing
+        days = optimize();
 
-		// output();
-	}
+        // 2.
+        boolean feasible = checkFeasible(days);
 
-	private static void ReadFileIn() {
 
-		machines = new ArrayList<>();
-		machines.add(new Machine());
-		machines.add(new Machine());
-		// days.get(2).getblock(7).getMachineStatus(machines.get(2));
-	}
 
-	private static void metaHeuristic() {
-		int solution = 0;
-		int teller = 0;
-		while (teller < 1000) {
-			int value = localSearch();
-			if (solution < value) {
-				solution = value;
-				teller = 0;
-			} else {
-				teller++;
-			}
-		}
-	}
-	private static int localSearch() {
-		//some localsearch thing.
-		return 1;/*dns × pn + to × po +
+
+        ReadFileIn();
+
+
+        if (feasible) {
+            evaluate(days);
+        }
+        // metaheuristik();
+
+        // output();
+    }
+
+    private static List<Day> optimize() {
+
+        return new ArrayList<Day>();
+    }
+
+    private static boolean checkFeasible(List<Day> days) {
+
+        return true;
+    }
+
+    private static void evaluate(List<Day> solution) {
+
+
+    }
+
+
+    private static void ReadFileIn() {
+
+        machines = new ArrayList<>();
+        machines.add(new Machine());
+        machines.add(new Machine());
+        // days.get(2).getblock(7).getMachineStatus(machines.get(2));
+    }
+
+    private static void metaHeuristic() {
+        int solution = 0;
+        int teller = 0;
+        while (teller < 1000) {
+            int value = localSearch();
+            if (solution < value) {
+                solution = value;
+                teller = 0;
+            } else {
+                teller++;
+            }
+        }
+    }
+
+    private static int localSearch() {
+        //some localsearch thing.
+        return 1;/*dns × pn + to × po +
 		X
 		r∈V
 		X
@@ -55,6 +86,6 @@ public class Planner {
 		r × ci) + X
 		d∈D
 		(ud × ps) + dp × pp*/
-	}
+    }
 
 }
