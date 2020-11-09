061324fc3e5187ae54c2c3e94acb8fcb55838909
Romeo Permentier <56132002+ro-per@users.noreply.github.com>
2020-11-09 11:30:44 +0100

    update .gitignore for intellij

--- src/Planner.java
+++ src/Planner.java
@@ -11,7 +11,7 @@ public class Planner {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("#Verwijder jouw lijn als je kan bewerken op GIT.");
-		System.out.println("\t Romeo Permentier");
+//		System.out.println("\t Romeo Permentier");
 		System.out.println("\tNick Braeckman");
 		System.out.println("\tElke Govaert");
 
