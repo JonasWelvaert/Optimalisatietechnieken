022d5f70ddc83c77522611a2d4926f245ca77670
JonasWelvaert <jonas.welvaert@gmail.com>
2020-11-09 10:17:28 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		System.out.println("#Verwijder jouw lijn als je kan bewerken op GIT.");
+		System.out.println("\t Romeo Permentier");
+		System.out.println("\tNick Braeckman");
+		System.out.println("\tJonas Welvaert");
+		System.out.println("\tElke Govaert");
+	}
+
+}
