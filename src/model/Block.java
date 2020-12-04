a2aa946fdfb5a78a43c6c086f0b64e4bbe642360
JonasWelvaert <jonas.welvaert@gmail.com>
2020-12-04 13:20:37 +0100

    toString Block for debugging

--- src/model/Block.java
+++ src/model/Block.java
@@ -7,37 +7,47 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Block {
-    private final int id;
-    private final Map<Machine, MachineState> machineState;
+	private final int id;
+	private final Map<Machine, MachineState> machineState;
 
-    public Block(int id) {
-        this.id = id;
-        machineState = new HashMap<>();
-    }
+	public Block(int id) {
+		this.id = id;
+		machineState = new HashMap<>();
+	}
 
-    public Block(Block b) {
-        this.id = b.id;
-        this.machineState = new HashMap<>(b.machineState.size());
-    }
+	public Block(Block b) {
+		this.id = b.id;
+		this.machineState = new HashMap<>(b.machineState.size());
+	}
 
-    public void setMachineState(Machine m, MachineState ms) {
-        machineState.put(m, ms);
-    }
+	public void setMachineState(Machine m, MachineState ms) {
+		machineState.put(m, ms);
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public MachineState getMachineState(Machine m) {
-        return machineState.get(m);
-    }
+	public MachineState getMachineState(Machine m) {
+		return machineState.get(m);
+	}
 
-    public boolean isInMaintenance() {
-        for (MachineState ms : machineState.values()) {
-            if (ms.getClass() == Maintenance.class) {
-                return true;
-            }
-        }
-        return false;
-    }
+	public boolean isInMaintenance() {
+		for (MachineState ms : machineState.values()) {
+			if (ms.getClass() == Maintenance.class) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		String ret = "B" + id+"[";
+		for (Machine m : machineState.keySet()) {
+			ret += "M[" + m.getId() + " " + machineState.get(m).toString() + "]";
+		}
+		ret+="]";
+		return ret;
+	}
 }
